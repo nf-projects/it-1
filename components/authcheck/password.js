ec79f87fa127e334fbef585f166743847941dd96
Spectre <nf-projects@users.noreply.github.com>
2022-10-07 15:31:43 -0700

    better password check

--- components/authcheck/password.js
+++ components/authcheck/password.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const checkPassword = (val) => {
-  return (val.length > 8)
+  if(val.length < 8) { return false }
+
+  //check if it contains a number
+  var numbers = /\d/;  //regex
+  if(!numbers.test(val)) { return false }
+  
+  //check if it contains a special character
+  var special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+  if(!special.test(val)) { return false }
+  
+  return true
 }
 
 export default checkPassword
