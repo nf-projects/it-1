9fd11d26c226ac2f1aedf0a5c1d8429e056faa2c
nf-projects <90344282+nf-projects@users.noreply.github.com>
2022-10-07 20:29:53 -0700

    add tlds to email check

--- util/authcheck/email.js
+++ util/authcheck/email.js
@@ -1,12 +1,17 @@
 import React from 'react'
 
-const checkEmail = (val) => {
+const checkEmailRegex = (val) => {
   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)) {
     // I stole this code from https://www.w3resource.com/javascript/form/email-validation.php
     return (true)
   } else {
     return (false)
   }
+}
+
+const checkEmail = (val) => {
+  let input = val.toLowerCase()
+  
 }
 
 export default checkEmail
