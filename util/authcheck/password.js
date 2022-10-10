06e5313b800cccd6640debca624c30be9300fe89
Spectre <nf-projects@users.noreply.github.com>
2022-10-10 15:43:47 -0700

    format

--- util/authcheck/password.js
+++ util/authcheck/password.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
 const checkPassword = (val) => {
-  if(val.length < 8) { return false }
+  if (val.length < 8) {
+    return false;
+  }
 
   //check if it contains a number
-  var numbers = /\d/;  //regex
-  if(!numbers.test(val)) { return false }
-  
+  var numbers = /\d/; //regex
+  if (!numbers.test(val)) {
+    return false;
+  }
+
   //check if it contains a special character
   var special = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-  if(!special.test(val)) { return false }
-  
-  return true
+  if (!special.test(val)) {
+    return false;
+  }
+
+  return true;
 }
 
 export default checkPassword
